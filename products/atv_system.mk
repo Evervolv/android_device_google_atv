92d8dfc5fed6f81ed215af6ce524a5b415852d0a
Inseob Kim <inseob@google.com>
2024-04-17 18:20:48 +0900

    Build audio_effects.xml with Soong
    Bug: 335369205
    Test: build
    Change-Id: Ibcc24bef80c37a1650c3caf139db5dc9a85b8b6e

--- products/atv_system.mk
+++ products/atv_system.mk
@@ -117,5 +117,4 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     device/google/atv/permissions/tv_core_hardware.xml:system/etc/permissions/tv_core_hardware.xml
 
-PRODUCT_COPY_FILES += \
-    frameworks/av/media/libeffects/data/audio_effects.xml:system/etc/audio_effects.xml
+PRODUCT_PACKAGES += framework-audio_effects.xml
